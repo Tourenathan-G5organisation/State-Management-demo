4d88572da833106724c52ca20859cf6e59a85415
TOURE NATHAN <toure21@live.com>
2020-01-02 15:59:04 +0300

    Created the project\n Added the state management libraries Updated the main page

---
+++ README.md
@@ -0,0 +1,10 @@
+# demo
+
+Application to show how to use flutter with state management packages
+
+
+# Getting Started
+
+We implemented both Provider and Bloc State management.
+
+
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0xff2B2B28),
+        accentColor: Color(0xffE3BD06),
+      ),
+      home: MyHomePage(title: 'State Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Choose State management pattern',
+                  style: TextStyle(fontSize: 22.0),
+                ),
+                SizedBox(height: 20.0),
+                SizedBox(width: 200, height: 40,
+                  child:  RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Provider',
+                        style: TextStyle(fontSize: 18.0, ),
+                      ),
+                      color: Theme.of(context).accentColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(20.0),
+                      )),)
+
+              /*  RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Provider',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20.0),
+                    ))*/,
+                SizedBox(height: 20.0),
+                SizedBox(width: 200, height: 40,
+                child:  RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'BLoC Pattern',
+                      style: TextStyle(fontSize: 18.0, ),
+                    ),
+                    color: Theme.of(context).accentColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20.0),
+                    )),)
+
+              ],
+            ),
+          )),
+    );
+  }
+}
